4137e08df15e2b761c2676b121ed3932d23b5d6c
Tony <kavinjsir@gmail.com>
2022-01-30 19:57:45 -0500

    feat: Initial: crawl news

---
+++ gnews/spiders/gnews.py
@@ -0,0 +1,21 @@
+import scrapy
+import re
+import json
+
+class QuotesSpider(scrapy.Spider):
+    name = "gnews"
+
+    keyword1 = "booster"
+    keyword2 = "omnicron"
+
+    start_urls = [
+        f'https://news.google.com/search?q={keyword1}+{keyword2}&hl=en-US&gl=US&ceid=US%3Aen'
+    ]
+
+    def parse(self, response):
+        titles = re.findall(r'<h3 class="[^"]+?"><a[^>]+?>(.+?)</a>', response.text)
+
+        filename = f'{self.keyword1}_{self.keyword2}.json'
+        with open(filename, 'w') as f:
+            json.dump(titles, f)
+        self.log(f'Saved file {filename}')
