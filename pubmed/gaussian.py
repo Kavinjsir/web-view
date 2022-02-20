41b2f23f66772e1c4c66e042cb2a76cc453fc963
Tony <kavinjsir@gmail.com>
2022-02-20 02:39:04 -0500

    feat: Display keyword frequency by month/year

---
+++ pubmed/gaussian.py
@@ -0,0 +1,76 @@
+from peewee import *
+from playhouse.db_url import connect
+
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+import numpy as np
+
+import os
+from datetime import datetime
+
+KEYWORD = os.environ['KEYWORD']
+
+DB_URL = os.environ.get('DATABASE')
+db = connect(DB_URL)
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Dataset(BaseModel):
+    date = DateField()
+    title = TextField()
+    abstract = TextField()
+    keyword = TextField()
+
+query = Dataset.raw("""SELECT
+    date_trunc('month', date) m,
+    COUNT (id)
+FROM
+    dataset
+WHERE
+    keyword = %s
+GROUP BY
+    m
+ORDER BY
+    m;""", KEYWORD)
+
+COUNT_DICT = {}
+
+for item in query:
+    COUNT_DICT[item.m] = item.count
+
+# Create figure and plot space
+fig, ax = plt.subplots(figsize=(12, 12))
+
+# Add x-axis and y-axis
+ax.bar(COUNT_DICT.keys(),
+       COUNT_DICT.values(),
+       width=10)
+
+# Set title and labels for axes
+ax.set(xlabel="Year 2019-2022",
+       ylabel="Count",
+       title="Keyword: {}".format(KEYWORD))
+
+# Define the date format
+date_form = DateFormatter('%Y-%m')
+ax.xaxis.set_major_formatter(date_form)
+plt.show()
+
+
+'''
+count2021 = Dataset.select().where(Dataset.keyword == KEYWORD, Dataset.date >= datetime(2021, 1, 1), Dataset.date < datetime(2022, 1, 1)).count()
+
+count_dict = { 2019: count2019, 2020: count2020, 2021: count2021, 2022: count2022 }
+
+plt.bar(count_dict.keys(), count_dict.values(), 0.2, color='g')
+
+plt.ylabel("Frequent")
+plt.xlabel("Year")
+plt.title(KEYWORD);
+
+plt.show()
+
+'''
+
